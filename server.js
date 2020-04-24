974abe239839742a79f76eb9d338f118bb08f17e
Brent Cralley <brentcralley@gmail.com>
2020-04-24 02:01:34 -0500

    Messed with server.js

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const path = require('path')
 
 const app = express()
 
-app.use(function (req, res) {
-	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', 'http://www.brentcralley.com/');
-})
-
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+app.use(function (req, res) {
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', 'http://www.brentcralley.com/');
 })
 
 const port = process.env.PORT || 8081
