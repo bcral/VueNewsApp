74b679f328703ef1a8f9249f670f8aa0f6be2a47
Brent Cralley <brentcralley@gmail.com>
2020-04-24 01:47:46 -0500

    Added cross-origin options in server.js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const path = require('path')
 
 const app = express()
 
+app.use(function (req, res) {
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+})
+
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
